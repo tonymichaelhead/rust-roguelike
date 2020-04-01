0482ab8bed701918ac53a3c0f6e57fdd16a087ff
Tony Head <tony.head@dws.la>
2020-04-01 14:15:28 -0700

    Add player and npc to objects array

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,34 @@ struct Tcod {
     con: Offscreen,
 }
 
-fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+// This is a generic object: the player, a monster, an item, the stairs...
+// It's alway represented by a character on the screen.
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object { x, y, char, color }
+    }
+
+    // move by the given amount
+    pub fn move_by(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+    // set the color and then draw the cahracter that represents this object at its position
+    pub fn draw(&self, con: &mut dyn Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+}
+
+fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
@@ -28,18 +55,21 @@ fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool 
             tcod.root.set_fullscreen(!fullscreen);
         }
         Key { code: Escape, .. } => return true, // exit game
-        Key { code: Up, .. } => *player_y -= 1,
-        Key { code: Down, .. } => *player_y += 1,
-        Key { code: Left, .. } => *player_x -= 1,
-        Key { code: Right, .. } => *player_x += 1,
+        Key { code: Up, .. } => player.move_by(0, -1),
+        Key { code: Down, .. } => player.move_by(0, 1),
+        Key { code: Left, .. } => player.move_by(-1, 0),
+        Key { code: Right, .. } => player.move_by(1, 0),
 
         _=> {}
     }
-    // todo: handle kes
+
     false
 }
 
+
 fn main() {
+    tcod::system::set_fps(LIMIT_FPS);
+
     let root = Root::initializer()
         .font("arial10x10.png", FontLayout::Tcod)
         .font_type(FontType::Greyscale)
@@ -51,15 +81,23 @@ fn main() {
 
     let mut tcod = Tcod { root, con };
 
-    tcod::system::set_fps(LIMIT_FPS);
+    // create object representing the player
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
 
-    let mut player_x = SCREEN_WIDTH / 2;
-    let mut player_y = SCREEN_HEIGHT / 2;
+    // create an NPC
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
+
+    // the list of objects with those two
+    let mut objects = [player, npc];
 
     while !tcod.root.window_closed() {
-        tcod.con.set_default_foreground(WHITE);
+        // clear the screen of the previous frame
         tcod.con.clear();
-        tcod.con.put_char(player_x, player_y, '@', BackgroundFlag::None);
+
+        for object in &objects {
+            object.draw(&mut tcod.con);
+        }
+
         // blit the contents of "con" to the root consoles and present it
         blit(
             &tcod.con,
@@ -71,10 +109,10 @@ fn main() {
             1.0,
         );
         tcod.root.flush();
-        tcod.root.wait_for_keypress(true);
 
         // handle keys and exit game if needed
-        let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut tcod, player);
         if exit {
             break;
         }
